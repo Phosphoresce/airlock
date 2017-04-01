ae5cc3d61d48722fedaa182729bfa483fd4bd56f
phosphoric <phosphoresce.k@gmail.com>
2017-03-31 21:07:30 -0500

    so long and thanks for all the fish

--- airlock/main.go
+++ airlock/main.go
@@ -81,36 +81,35 @@ func listen(c *circle) {
 		if err != nil {
 			log.Print("Failed to read from socket: ", err)
 		} else {
-			fmt.Printf("received: %v bytes from %v.\nmessage: %s\n", rlen, remote, buff)
+			if strings.Contains(string(buff[:rlen]), ">") {
+				fmt.Printf("%v %s\n", remote.IP.String(), buff)
+			} else {
+				fmt.Printf("New connect from %v\n", remote)
+				// this response should be a list of ips and ports for all peers BESIDES the currently connected peer and the local peer
+				// can avoid appending the connected peer to the list until after we have sent the peer list barring peer[0]
+				// build the peer list
+				fmt.Printf("Peer objects: %v\n", c.peers)
+				if len(c.peers) > 1 {
+					peerlist := make([]string, 0)
+					for _, peer := range c.peers[1:] {
+						peerlist = append(peerlist, peer.addr.String())
+					}
 
-			// this response should be a list of ips and ports for all peers BESIDES the currently connected peer and the local peer
-			// can avoid appending the connected peer to the list until after we have sent the peer list barring peer[0]
-			// build the peer list
-			fmt.Println(c.peers)
-			if len(c.peers) > 1 {
-				peerlist := make([]string, 0)
-				for _, peer := range c.peers[1:] {
-					peerlist = append(peerlist, peer.addr.String())
+					fmt.Printf("sent peerlist: %s\n", strings.Join(peerlist, ","))
+					listener.WriteTo([]byte(strings.Join(peerlist, ",")), remote)
+				} else {
+					listener.WriteTo([]byte("nil"), remote)
 				}
 
-				fmt.Printf("peerlist: %s\n", strings.Join(peerlist, ","))
-				listener.WriteTo([]byte(strings.Join(peerlist, ",")), remote)
-			} else {
-				listener.WriteTo([]byte("nil"), remote)
+				// is remote already a peer? if not add it as a peer
+				remotePort, _ := strconv.Atoi(string(buff[:rlen]))
+
+				if peerExists(c, remote.IP.String(), remotePort) == -1 {
+					c.peers = append(c.peers, newPeer(remote.IP.String(), remotePort))
+				}
 			}
 
-			// TODO: is remote already a peer? if not add it as a peer
-			remotePort, _ := strconv.Atoi(string(buff[:rlen]))
-			c.peers = append(c.peers, newPeer(remote.IP.String(), remotePort))
 		}
-	}
-}
-
-// TODO: Send a message to all peers besides self
-func chat(c *circle) {
-	scanner := bufio.NewScanner(os.Stdin)
-	for scanner.Scan() {
-		fmt.Printf("Send: %v to %v", scanner.Text(), c.peers[1:])
 	}
 }
 
@@ -122,7 +121,7 @@ func connect(c *circle) {
 	}
 	defer client.Close()
 
-	// TODO: Send port number this client will listen on.
+	// Send port number this client will listen on.
 	buff := []byte(strconv.Itoa(c.peers[0].addr.Port))
 	_, err = client.Write(buff)
 	if err != nil {
@@ -140,7 +139,36 @@ func connect(c *circle) {
 		for _, peer := range peerlist {
 			peerAddr := strings.Split(peer, ":")
 			peerPort, _ := strconv.Atoi(peerAddr[len(peerAddr)-1])
-			c.peers = append(c.peers, newPeer(peerAddr[0], peerPort))
+
+			if peerExists(c, peerAddr[0], peerPort) == -1 {
+				c.peers = append(c.peers, newPeer(peerAddr[0], peerPort))
+				client2, _ := net.DialUDP("udp", nil, c.peers[len(c.peers)-1].addr)
+				defer client2.Close()
+				client2.Write(buff)
+			}
+		}
+	}
+}
+
+func peerExists(c *circle, ip string, port int) int {
+	fmt.Printf("checking for %v:%v\n", ip, port)
+	for i, peer := range c.peers {
+		if peer.addr.IP.String() == ip && peer.addr.Port == port {
+			return i
+		}
+	}
+	return -1
+}
+
+// Send a message to all peers besides self
+func chat(c *circle) {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		buffer := scanner.Text()
+		for _, peer := range c.peers[1:] {
+			client, _ := net.DialUDP("udp", nil, peer.addr)
+			client.Write([]byte("> " + buffer))
+			client.Close()
 		}
 	}
 }
